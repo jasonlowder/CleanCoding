159c0ad2ad581b74d1facf7840da5c9d78244bda
kris.meagan@gmail.com <kris.meagan@gmail.com>
2013-03-12 20:16:35 -0600

    changed some css

--- CleanCoding/Global.asax.cs
+++ CleanCoding/Global.asax.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using NotAClue.Web.DynamicData;
 
 namespace CleanCoding
 {
