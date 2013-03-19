79fec5e5bc45bd84be39248bf3a078fc671f15ca
kris.meagan@gmail.com <kris.meagan@gmail.com>
2013-03-18 19:13:08 -0600

    lglj

--- CleanCoding/Controllers/CommentController.cs
+++ CleanCoding/Controllers/CommentController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
+﻿using System.Data;
 using System.Web.Mvc;
 using CleanCoding.Models;
 
