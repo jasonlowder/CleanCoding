505f8b0d6016a6d704179f6a91c3e1116af65c61
TyrelColt <TyrelColt@gmail.com>
2013-03-20 19:38:31 -0600

    Hey

--- CleanCoding/Models/CleanCodingDB.cs
+++ CleanCoding/Models/CleanCodingDB.cs
@@ -6,14 +6,25 @@ using System.Web;
 
 namespace CleanCoding.Models
 {
-    public class CleanCodingDB : DbContext
+    public interface ICleanCodingDB : IDisposable
     {
-        public CleanCodingDB() : base("name=DefaultConnection")
+        IQueryable<T> Query<T>() where T : class;
+    }
+
+    public class CleanCodingDB : DbContext, ICleanCodingDB
+    {
+        public CleanCodingDB()
+            : base("name=DefaultConnection")
         {
 
         }
 
         public DbSet<Article> Articles { get; set; }
         public DbSet<Comment> Comments { get; set; }
+
+        IQueryable<T> ICleanCodingDB.Query<T>()
+        {
+            return Set<T>();
+        }
     }
 }
