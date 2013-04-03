eab78bf98d6b295beed6f8c4aaa5df0700f085c0
TyrelColt <TyrelColt@gmail.com>
2013-04-02 22:12:40 -0600

    adding

---
+++ CleanCoding.Tests/Controllers/Comments/Comments_Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CleanCoding.Tests.Controllers.Comments
+{
+    [TestClass]
+    public class Comments_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
